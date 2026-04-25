0d7dc413a182148c50f260b4526641ae3bbc33a5
Eslam-Mohamed-1892 <eslameldakhli20@gmail.com>
2026-04-25 14:37:02 +0300

    upgrade

--- src/pages/MainHeader.jsx
+++ src/pages/MainHeader.jsx
@@ -3,23 +3,24 @@ import logo from '../images/audiophile 2.png'
 import { HiOutlineShoppingCart } from "react-icons/hi";
 
 export default function MainHeader() {
-  let headerStyle = 'w-full z-10 bg-[#0E0E0E] flex justify-center'
-  let containerStyle = 'container w-277.5 text-white flex items-center justify-center pt-8 pb-9 border-b border-[#FFFFFF]'
+
+
   let imgStyle = 'w-35.75 h-6.25'
   let navStyle = 'w-107.25 h-6.25 flex gap-8.5 ml-49.25 mr-[317.5px]'
   let aStyle = "hover:text-black transition duration-500 hover:bg-white uppercase text-[13px] font-bold cursor-pointer"
-  let iconStyle = 'w-[23.33px] h-[15.83px]'
+
   return (
-    <header className={headerStyle}>
-      <div className={containerStyle}>
+    <header className='w-full z-10 bg-[#0E0E0E] flex justify-center'>
+      <div className='container w-277.5 text-white flex items-center justify-center pt-8 pb-9 border-b border-[#FFFFFF]'>
         <img className={imgStyle} src={logo} alt="" />
         <nav className={navStyle}>
           <a className={aStyle}>Home</a>
           <a className={aStyle}>Headphones</a>
           <a className={aStyle}>Speakers</a>
           <a className={aStyle}>Earphones</a>
-        </nav><HiOutlineShoppingCart className={iconStyle} />
+        </nav><HiOutlineShoppingCart className='w-[23.33px] h-[15.83px] sm:flex-none' />
       </div>
     </header>
   )
 }
+
--- src/pages/Section1.jsx
+++ src/pages/Section1.jsx
@@ -2,24 +2,17 @@ import React from 'react'
 import headphone from '../images/headphone.png'
 
 export default function Section1() {
-    let sectionStyle ='w-full h-dvh bg-[#0E0E0E] overflow-hidden flex justify-center'
-    let containerStyle = 'container w-277.5 flex flex-row justify-between'
-    let divStyle = 'left text-[#FFFFFF] w-99.5 h-86.5'
-    let p1Style = 'tracking-[10px] leading-[100%] font-normal uppercase pt-32 pb-6 opacity-50'
-    let h1Style = 'text-[56px] font-bold leading-14.5 tracking-[2px] uppercase pb-6'
-    let p2Style = 'font-normal text-[15px] leading-6.25 pb-10 opacity-75'
-    let buttonStyle = 'bg-[#D87D4A] w-40 h-12 font-bold text-[13px] leading-[100%] tracking-[1px] mb-40 hover:bg-black hover:text-white active:bg-amber-500  active:text-black cursor-pointer'
-    let imgStyle = 'w-168.75 h-211'
+
     return (
-        <section className={sectionStyle}>
-            <div className={containerStyle}>
-                <div className={divStyle}>
-                    <p className={p1Style}>NEW PRODUCT</p>
-                    <h1 className={h1Style}>XX99 Mark II<br />Headphones</h1>
-                    <p className={p2Style}>Experience natural, lifelike audio and exceptional<br />build quality made for the passionate music<br />enthusiast.</p>
-                    <button className={buttonStyle}>See Pruduct</button>
+        <section className='w-full h-dvh bg-[#0E0E0E] overflow-hidden flex justify-center'>
+            <div className='container w-277.5 flex flex-row justify-between'>
+                <div className='left text-[#FFFFFF] w-99.5 h-86.5'>
+                    <p className='tracking-[10px] leading-[100%] font-normal uppercase pt-32 pb-6 opacity-50'>NEW PRODUCT</p>
+                    <h1 className='text-[56px] font-bold leading-14.5 tracking-[2px] uppercase pb-6'>XX99 Mark II<br />Headphones</h1>
+                    <p className='font-normal text-[15px] leading-6.25 pb-10 opacity-75'>Experience natural, lifelike audio and exceptional<br />build quality made for the passionate music<br />enthusiast.</p>
+                    <button className='bg-[#D87D4A] w-40 h-12 font-bold text-[13px] leading-[100%] tracking-[1px] mb-40 hover:bg-black hover:text-white active:bg-amber-500  active:text-black cursor-pointer'>See Pruduct</button>
                 </div>
-                <img className={imgStyle} src={headphone} alt="" />
+                <img className='w-168.75 h-211' src={headphone} alt="" />
             </div>
         </section>
     )
